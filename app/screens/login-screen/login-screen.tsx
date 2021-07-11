207f0f55cee190af08bf727f65d837ef4ada26e1
Macintosh <auduongvanhieu@gmail.com>
2021-07-11 18:36:33 +0700

    add warn text

--- app/screens/login-screen/login-screen.tsx
+++ app/screens/login-screen/login-screen.tsx
@@ -7,6 +7,7 @@ import metrics from "../../theme/metrics"
 import { Icon } from "react-native-elements/dist/icons/Icon"
 import { useNavigation } from "@react-navigation/native"
 import { Api } from "../../services/api"
+import { WarnText } from "../../components/text/warn-text"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.main,
@@ -14,23 +15,41 @@ const ROOT: ViewStyle = {
 }
 
 export const LoginScreen = observer(function LoginScreen() {
-  
+
   const [phone, setPhone] = useState('0909000100');
+  const [warnPhone, setWarnPhone] = useState(undefined);
   const [password, setPassword] = useState('12345');
+  const [warnPassword, setWarnPassword] = useState(undefined);
   const [isShowPassword, setShowPassword] = useState(false);
 
   const navigation = useNavigation()
   const goToMain = () => { navigation.navigate("main") }
   const goToRegister = () => { navigation.navigate("registerStep1") }
 
-  const login = async () => {
-    const api = new Api()
-    api.setup()
-    let res = await api.login({phone,password})
-    if(res.kind == 'ok'){
-      goToMain()
+  const checkValidInput = () => {
+    var isValid = true
+    setWarnPhone(undefined)
+    setWarnPassword(undefined)
+    if (phone.length < 10) {
+      setWarnPhone('Vui lòng nhập đúng số điện thoại')
+      isValid = false
     }
-    console.log('hieunv', 'loginRes', res);
+    if (password.length < 5) {
+      setWarnPassword('Vui lòng nhập password có ít nhất 5 ký tự')
+      isValid = false
+    }
+    return isValid
+  }
+
+  const login = async () => {
+    if (checkValidInput()) {
+      const api = new Api()
+      api.setup()
+      let res = await api.login({ phone, password })
+      if (res.kind == 'ok') {
+        goToMain()
+      }
+    }
   }
 
   return (
@@ -39,30 +58,34 @@ export const LoginScreen = observer(function LoginScreen() {
         <Text preset="header" text="ĐĂNG NHẬP" />
       </View>
       <View style={{ flex: 1, marginHorizontal: metrics.baseMargin }}>
+        {/* Số điện thoại */}
         <Text preset="default" text="Số điện thoại" />
         <TextField
-          componentLeft={<Icon type='font-awesome-5' name='user-tie' containerStyle={{marginStart: 5}} size={16}/>}
+          componentLeft={<Icon type='font-awesome-5' name='user-tie' containerStyle={{ marginStart: 5 }} size={16} />}
           placeholder='Nhập số điện thoại' onChangeText={phone => setPhone(phone)} defaultValue={phone} keyboardType='number-pad' />
+        <WarnText preset="default" text={warnPhone} />
+        {/* Mật khẩu */}
         <Text preset="default" text="Mật khẩu" style={{ marginTop: 10 }} />
         <TextField
-          componentLeft={<Icon type='font-awesome-5' name='lock' containerStyle={{marginStart: 5}} size={16}/>}
-          componentRight={<Icon onPress={()=>setShowPassword(!isShowPassword)} type='entypo' name={isShowPassword?'eye':'eye-with-line'} containerStyle={{marginEnd: 5}} size={18}/>}
-          placeholder='Nhập mật khẩu' onChangeText={password => setPassword(password)} defaultValue={password} secureTextEntry={!isShowPassword}/>
-        <View style={{marginTop: 15}}>
+          componentLeft={<Icon type='font-awesome-5' name='lock' containerStyle={{ marginStart: 5 }} size={16} />}
+          componentRight={<Icon onPress={() => setShowPassword(!isShowPassword)} type='entypo' name={isShowPassword ? 'eye' : 'eye-with-line'} containerStyle={{ marginEnd: 5 }} size={18} />}
+          placeholder='Nhập mật khẩu' onChangeText={password => setPassword(password)} defaultValue={password} secureTextEntry={!isShowPassword} />
+        <WarnText preset="default" text={warnPassword}/>
+        <View style={{ marginTop: 15 }}>
           <Button onPress={login} text="ĐĂNG NHẬP" style={{ backgroundColor: 'black', height: 40 }} textStyle={{ fontSize: 15 }} />
         </View>
         <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-          <TouchableOpacity onPress={goToRegister} style={{flex: 1, alignItems: 'center', paddingVertical: 5}}>
-            <Text preset="bold" text="TẠO TÀI KHOẢN" />
+          <TouchableOpacity onPress={goToRegister} style={{ flex: 1, alignItems: 'center', paddingVertical: 5 }}>
+            <Text preset="bold" text="TẠO TÀI KHOẢN" style={{ textDecorationLine: 'underline' }} />
           </TouchableOpacity>
           <Text preset="bold" text="|" />
-          <TouchableOpacity style={{flex: 1, alignItems: 'center', paddingVertical: 5}}>
-            <Text preset="bold" text="QUÊN MẬT KHẨU" />
+          <TouchableOpacity style={{ flex: 1, alignItems: 'center', paddingVertical: 5 }}>
+            <Text preset="bold" text="QUÊN MẬT KHẨU" style={{ textDecorationLine: 'underline' }} />
           </TouchableOpacity>
         </View>
       </View>
       <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', marginHorizontal: metrics.baseMargin }}>
-        <Text preset="default" text="version: 1.0.0" style={{fontSize: 12}} />
+        <Text preset="default" text="version: 1.0.0" style={{ fontSize: 12 }} />
       </View>
     </Screen>
   )
--- app/screens/register-screen/register-step-1-screen.tsx
+++ app/screens/register-screen/register-step-1-screen.tsx
@@ -5,6 +5,7 @@ import { Button, Screen, Text, TextField } from "../../components"
 import { color } from "../../theme"
 import { Icon } from "react-native-elements"
 import metrics from "../../theme/metrics"
+import { WarnText } from "../../components/text/warn-text"
 
 const ROOT: ViewStyle = {
   backgroundColor: color.main,
@@ -12,7 +13,24 @@ const ROOT: ViewStyle = {
 }
 
 export const RegisterStep1Screen = observer(function RegisterStep1Screen() {
-  const [phone, setPhone] = useState('0909000100');
+  const [phone, setPhone] = useState('');
+  const [warnPhone, setWarnPhone] = useState(undefined);
+
+  const checkValidInput = () => {
+    var isValid = true
+    setWarnPhone(undefined)
+    if (phone.length < 10) {
+      setWarnPhone('Vui lòng nhập đúng số điện thoại')
+      isValid = false
+    }
+    return isValid
+  }
+
+  const getOtpRequest = async () => {
+    if (checkValidInput()) {
+      
+    }
+  }
 
   return (
     <Screen style={ROOT} preset="scroll">
@@ -20,13 +38,15 @@ export const RegisterStep1Screen = observer(function RegisterStep1Screen() {
         <Text preset="header" text="ĐĂNG KÝ" />
       </View>
       <View style={{ flex: 1, marginHorizontal: metrics.baseMargin }}>
-        <Text preset="default" text="Số điện thoại"/>
+        <Text preset="default" text="Số điện thoại" />
         <TextField
           componentLeft={<Icon type='font-awesome-5' name='user-tie' containerStyle={{ marginStart: 5 }} size={16} />}
           placeholder='Nhập số điện thoại' onChangeText={phone => setPhone(phone)} defaultValue={phone} keyboardType='number-pad' />
+        <WarnText preset="default" text={warnPhone} />
+
       </View>
       <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', marginHorizontal: metrics.baseMargin }}>
-        <Button text="BƯỚC TIẾP THEO" style={{ backgroundColor: 'black', height: 40, marginBottom: 20 }} textStyle={{ fontSize: 15 }} />
+        <Button onPress={getOtpRequest} text="BƯỚC TIẾP THEO" style={{ backgroundColor: 'black', height: 40, marginBottom: 20 }} textStyle={{ fontSize: 15 }} />
       </View>
     </Screen>
   )
