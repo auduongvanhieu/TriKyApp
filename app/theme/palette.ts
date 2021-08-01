9ff50975dc8280476e325c71c5f6759939d7fe4f
Macintosh <auduongvanhieu@gmail.com>
2021-08-01 12:17:46 +0700

    remove palette file

--- app/theme/palette.ts
+++
@@ -1,9 +0,0 @@
-export const palette = {
-  black: "#1d1d1d",
-  white: "#ffffff",
-  offWhite: "#e6e6e6",
-  orange: "#FBA928",
-  lightGrey: "#939AA4",
-  lighterGrey: "#CDD4DA",
-  angry: "#dd3333",
-}
